dbb817ac93b82f13b40abc95c3251d34b47fe51d
Mad Software Scientist <49042855+MadSoftwareScientist@users.noreply.github.com>
2019-04-07 00:05:51 +0300

    Fixed some style errors.

--- server/core/management/commands/wait_for_db.py
+++ server/core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
@@ -16,4 +17,3 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-        
